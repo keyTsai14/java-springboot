1c7f1c250c6c30e41ed0892f356859fbbaa21efb
caimingqi <gbafang2021@163.com>
2022-12-07 15:06:55 +0800

    自动配置原理流程

--- src/main/java/com/cmqSystem/boot/controller/HelloController.java
+++ src/main/java/com/cmqSystem/boot/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.cmqSystem.boot.controller;
 import com.cmqSystem.boot.bean.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +20,8 @@ public class HelloController {
     Car car;
 
     @RequestMapping("/hello")
-    public String handle01() {
-        return "Hello, String Boot 2!"+"您好";
+    public String handle01(@RequestParam("name")String name) {
+        return "Hello, String Boot 2!"+"您好 "+name;
     }
 
     @RequestMapping("/car")
