9911ae315fe446885ae40453409d665ffa02a351
caimingqi <gbafang2021@163.com>
2022-12-16 18:00:23 +0800

    常用参数注解使用

--- src/main/java/com/cmqSystem/boot/controller/ParameterTestController.java
+++ src/main/java/com/cmqSystem/boot/controller/ParameterTestController.java
@@ -1,7 +1,6 @@
 package com.cmqSystem.boot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,15 +10,27 @@ import java.util.Map;
  * @Author admin
  * @Date 2022/12/15
  **/
+@RestController
 public class ParameterTestController {
 
     @GetMapping("/car/{id}/owner/{username}")
     public Map<String, Object> getCar(@PathVariable("id") Integer id,
-                                      @PathVariable("username") String name) {
+                                      @PathVariable("username") String name,
+                                      @PathVariable Map<String, Object> pv,
+                                      @RequestHeader("User-Agent") String userAgent,
+                                      @RequestHeader Map<String,String> headers,
+                                      @CookieValue("token") String token) {
 
-        Map<String, Object> result = new HashMap<String, Object>();
+        Map<String, Object> result = new HashMap<>();
 
-        return null;
+//        result.put("id",id);
+//        result.put("name",name);
+//        result.put("pv",pv);
+//
+//        result.put("userAgent",userAgent);
+//        result.put("headers",headers);
+        result.put("token",token);
+        return result;
     }
 
 }
