fd942c6d4ad0d262e63a0a152b017d6f7dc672d5
caimingqi <gbafang2021@163.com>
2022-12-20 18:00:50 +0800

    响应处理 -- ReturnValueHandler

---
+++ src/main/java/com/cmqSystem/boot/controller/ResponseTestController.java
@@ -0,0 +1,27 @@
+package com.cmqSystem.boot.controller;
+
+import com.cmqSystem.boot.bean.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * @Description TODO
+ * @Author admin
+ * @Date 2022/12/20
+ **/
+@Controller
+public class ResponseTestController {
+
+    @ResponseBody
+    @GetMapping("/test/person")
+    public Person getPerson() {
+        Person person = new Person();
+        person.setAge(28);
+        person.setBirth(new Date());
+        person.setUserName("zhangsan");
+        return person;
+    }
+}
