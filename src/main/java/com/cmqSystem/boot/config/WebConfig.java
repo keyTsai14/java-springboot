c8de356261b80bf4af67df0d65f08d10c2cfeb50
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-22 23:15:01 +0800

    响应处理 -- url参数的形式完成 自定义MessageConverter 内容协商

--- src/main/java/com/cmqSystem/boot/config/WebConfig.java
+++ src/main/java/com/cmqSystem/boot/config/WebConfig.java
@@ -7,14 +7,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description TODO
@@ -42,6 +48,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
+
+            /**
+             * 配置内容协商功能
+             * */
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                Map<String, MediaType> mediaTypeMap = new HashMap<>();
+                // 指定支持解析哪些参数对应哪些媒体类型
+                mediaTypeMap.put("json",MediaType.APPLICATION_JSON);
+                mediaTypeMap.put("xml",MediaType.APPLICATION_XML);
+                mediaTypeMap.put("gg",MediaType.parseMediaType("application/x-guigu"));
+                ParameterContentNegotiationStrategy parameterContentNegotiationStrategy = new ParameterContentNegotiationStrategy(mediaTypeMap);
+
+                configurer.strategies(Arrays.asList(parameterContentNegotiationStrategy));
+            }
 
             @Override
             public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
