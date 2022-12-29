c90dd93c0f4285745ab93140b407507ff00f7aa5
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-29 18:56:41 +0800

    数据访问 -- redis

--- src/main/java/com/cmqSystem/boot/config/AdminWebConfig.java
+++ src/main/java/com/cmqSystem/boot/config/AdminWebConfig.java
@@ -1,6 +1,8 @@
 package com.cmqSystem.boot.config;
 
 import com.cmqSystem.boot.interceptor.LoginInterceptor;
+import com.cmqSystem.boot.interceptor.RedisUrlCountInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,10 +18,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
 
+
+    /**
+     * Filter、Interceptor几乎棚有相同的功能？
+     * 1、Filter是Servlet定义的原生组件。好处，脱离Spring应用也能使用
+     * 2、Interceptor是Spring定义的接口。可以使用Spring的自动装配等功能
+     *
+     * */
+    @Autowired
+    RedisUrlCountInterceptor redisUrlCountInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**") //所有清求都被拦截包括静态资源
                 .excludePathPatterns("/","/login","/Login.css","/sql");
+
+        registry.addInterceptor(redisUrlCountInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/login","/Login.css");
     }
 }
---
+++ src/main/java/com/cmqSystem/boot/interceptor/RedisUrlCountInterceptor.java
@@ -0,0 +1,29 @@
+package com.cmqSystem.boot.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author key
+ * @description
+ * @create: 2022-12-29 18:50
+ */
+@Component
+public class RedisUrlCountInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        //默认每次访问当前uri就会计数+1
+        redisTemplate.opsForValue().increment(uri);
+        return true;
+    }
+}
