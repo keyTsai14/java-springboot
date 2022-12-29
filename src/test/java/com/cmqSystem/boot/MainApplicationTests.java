d13532f3917786356a857fa5354db2e92e554bf4
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-29 18:44:14 +0800

    数据访问 -- redis操作

--- src/test/java/com/cmqSystem/boot/MainApplicationTests.java
+++ src/test/java/com/cmqSystem/boot/MainApplicationTests.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -34,6 +36,9 @@ public class MainApplicationTests {
     @Autowired
     UserService userService;
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
 
     @Test
     void contextLoads() {
@@ -53,4 +58,15 @@ public class MainApplicationTests {
         log.info("用户信息：{}",user);
         log.info("用户信息s：{}",list);
     }
+
+    @Test
+    void testRedis() {
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+
+        operations.set("hello","world");
+        String hello = operations.get("hello");
+        System.out.println(hello);
+
+    }
+
 }
