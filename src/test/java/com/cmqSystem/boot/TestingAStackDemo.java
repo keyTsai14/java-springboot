d6ae3f2c0dba5329635a32c40921ede6909e8277
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-29 22:48:53 +0800

    单元测试 -- 嵌套测试

---
+++ src/test/java/com/cmqSystem/boot/TestingAStackDemo.java
@@ -0,0 +1,94 @@
+package com.cmqSystem.boot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author key
+ * @description
+ * @create: 2022-12-29 22:32
+ */
+@DisplayName("嵌套测试")
+public class TestingAStackDemo {
+
+    Stack<Object> stack;
+
+    @Test
+    @DisplayName("new Stack()")
+    void isInstantiatedWithNew() {
+        new Stack<>();
+        //嵌套测试情况下，外层的Test不能驱动内层的Before(After)Each/ALL之类的方法提前/之后运行
+        assertNull(stack);
+    }
+
+    @Nested
+    @DisplayName("when new")
+    class WhenNew {
+
+        @BeforeEach
+        void createNewStack() {
+            stack = new Stack<>();
+        }
+
+        @Test
+        @DisplayName("is empty")
+        void isEmpty() {
+            assertTrue(stack.isEmpty());
+        }
+
+        @Test
+        @DisplayName("throws EmptyStackException when popped")
+        void throwsExceptionWhenPopped() {
+            assertThrows(EmptyStackException.class, stack::pop);
+        }
+
+        @Test
+        @DisplayName("throws EmptyStackException when peeked")
+        void throwsExceptionWhenPeeked() {
+            assertThrows(EmptyStackException.class, stack::peek);
+        }
+
+        @Nested
+        @DisplayName("after pushing an element")
+        class AfterPushing {
+
+            String anElement = "an element";
+
+            @BeforeEach
+            void pushAnElement() {
+                stack.push(anElement);
+            }
+
+            /**
+             * 内层Test可以驱动外层的Before(After)Each/ALL之类的方法提前/之后运行
+             * */
+            @Test
+            @DisplayName("it is no longer empty")
+            void isNotEmpty() {
+                assertFalse(stack.isEmpty());
+            }
+
+            @Test
+            @DisplayName("returns the element when popped and is empty")
+            void returnElementWhenPopped() {
+                assertEquals(anElement, stack.pop());
+                assertTrue(stack.isEmpty());
+            }
+
+            @Test
+            @DisplayName("returns the element when peeked but remains not empty")
+            void returnElementWhenPeeked() {
+                assertEquals(anElement, stack.peek());
+                assertFalse(stack.isEmpty());
+            }
+        }
+    }
+
+}
