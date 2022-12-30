0816537e81501303496d859b51d119af8897f8ab
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-30 11:01:50 +0800

    指标监控 -- 定制 Endpoint

---
+++ src/main/java/com/cmqSystem/boot/acutuator/endpoint/MyServiceEndpoint.java
@@ -0,0 +1,29 @@
+package com.cmqSystem.boot.acutuator.endpoint;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author key
+ * @description
+ * @create: 2022-12-30 10:58
+ */
+@Component
+@Endpoint(id = "myservice")
+public class MyServiceEndpoint {
+
+    @ReadOperation
+    public Map getDockerInfo(){
+        return Collections.singletonMap("info","docker started...");
+    }
+
+    @WriteOperation
+    private void restartDocker(){
+        System.out.println("docker restarted....");
+    }
+}
