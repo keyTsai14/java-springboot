6635822aeb7e42459e7fc61237bd10a12d80476a
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-30 10:25:43 +0800

    指标监控 -- 定制 Endpoint -- 定制 Health 信息

---
+++ src/main/java/com/cmqSystem/boot/health/MyComHealthIndicator.java
@@ -0,0 +1,39 @@
+package com.cmqSystem.boot.health;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author key
+ * @description
+ * @create: 2022-12-30 10:07
+ */
+@Component
+public class MyComHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        Map<String,Object> map = new HashMap<>();
+        // 检查完成
+        if(1 == 2){
+//            builder.up(); //健康
+            builder.status(Status.UP);
+            map.put("count",1);
+            map.put("ms",100);
+        }else {
+//            builder.down();
+            builder.status(Status.OUT_OF_SERVICE);
+            map.put("err","连接超时");
+            map.put("ms",3000);
+        }
+
+
+        builder.withDetail("code",100)
+                .withDetails(map);
+
+    }
+}
