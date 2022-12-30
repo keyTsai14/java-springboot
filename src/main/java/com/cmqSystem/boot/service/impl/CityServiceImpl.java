9021a82cc494f829b1e49631a9a7c27dc768165d
Bananakey14 <tsaimingchi14@outlook.com>
2022-12-30 10:56:47 +0800

    指标监控 -- 定制 Endpoint -- 增加定制Metrics

--- src/main/java/com/cmqSystem/boot/service/impl/CityServiceImpl.java
+++ src/main/java/com/cmqSystem/boot/service/impl/CityServiceImpl.java
@@ -2,6 +2,9 @@ package com.cmqSystem.boot.service.impl;
 
 import com.cmqSystem.boot.bean.City;
 import com.cmqSystem.boot.mapper.CityMapper;
+import com.cmqSystem.boot.service.CityService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,17 @@ import org.springframework.stereotype.Service;
  * @create: 2022-12-29 08:21
  */
 @Service
-public class CityServiceImpl implements com.cmqSystem.boot.service.CityService {
+public class CityServiceImpl implements CityService {
 
     @Autowired
     CityMapper cityMapper;
+
+
+    Counter counter;
+    public CityServiceImpl(MeterRegistry meterRegistry){
+        counter = meterRegistry.counter("cityService.saveCity.counter");
+    }
+
 
     @Override
     public City getById(Long id) {
@@ -24,6 +34,7 @@ public class CityServiceImpl implements com.cmqSystem.boot.service.CityService {
 
     @Override
     public void saveCity(City city) {
+        counter.increment();
         cityMapper.insert(city);
     }
 
